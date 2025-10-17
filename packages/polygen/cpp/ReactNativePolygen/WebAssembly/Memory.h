fefd095771772dc26da95642ebf8443e80d90005
owl352 <kokosinca123@gmail.com>
2025-10-17 22:53:26 +0300

    update wasm-rt and polygen cpp

--- packages/polygen/cpp/ReactNativePolygen/WebAssembly/Memory.h
+++ packages/polygen/cpp/ReactNativePolygen/WebAssembly/Memory.h
@@ -14,42 +14,42 @@ namespace callstack::polygen {
 class Memory: public facebook::jsi::NativeState, public facebook::jsi::MutableBuffer {
 public:
   explicit Memory(wasm_rt_memory_t* memory): memory_(memory) {}
-  
+
   Memory(uint64_t initial, uint64_t maximum, bool is64 = false) {
     this->memory_ = &this->ownedMemory_;
-    wasm_rt_allocate_memory(this->memory_, initial, maximum, is64);
+    wasm_rt_allocate_memory(this->memory_, initial, maximum, is64, 65536);
   }
-  
+
   virtual ~Memory() {
     if (this->isOwned()) {
       wasm_rt_free_memory(this->memory_);
     }
   }
-  
+
   const wasm_rt_memory_t* getMemory() const {
     return this->memory_;
   }
-  
+
   wasm_rt_memory_t* getMemory() {
     return this->memory_;
   }
-  
+
   bool isOwned() const {
     return this->memory_ == &this->ownedMemory_;
   }
-  
+
   void grow(uint64_t delta) const {
     wasm_rt_grow_memory(this->memory_, delta);
   }
-  
+
   size_t size() const {
     return memory_->size;
   }
-  
+
   uint8_t* data() {
     return memory_->data;
   }
-  
+
 private:
   wasm_rt_memory_t* memory_;
   wasm_rt_memory_t ownedMemory_;
